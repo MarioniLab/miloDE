5e259ba16769cc729f0a5bb601595983799dcbce
amissarova <alsu.missarova.32@gmail.com>
2022-08-17 17:54:10 +0100

    add exmaple

--- R/de_test_all_hoods.R
+++ R/de_test_all_hoods.R
@@ -7,8 +7,6 @@
 #' Tests all hoods for DE + performs spatialFDR correction after
 #' @param sce Milo object
 #' @param genes Subset of rownames(sce) for which we will perform DE testing. Default = rownames(sce)
-#' @param nhoods_sce Can be extracted from sce as nhoods(sce) prior to running the function. The reason the argument is passed by itself is to avoid calculating it every time.
-#' @param hood.id Character specifying for which hood we should perform testing. Should be in colnames(nhoods_sce)
 #' @param sample.id Character specifying which variable should be used as a sample/replica id. Should be in colData(sce)
 #' @param condition.id Character specifying which variable should be used as a condition id. Should be in colData(sce)
 #' @param discard_not_perturbed_hoods Boolean specifying whether perform prior hood selection using Augur based classifier. Note for big datasets it might take a while so recommended if a large portion of hoods is expected to be unperturbed.
@@ -27,11 +25,24 @@
 #' @importFrom scuttle summarizeAssayByGroup
 #'
 #' @examples
+#' require(SingleCellExperiment)
+#' n_row = 500
+#' n_col = 100
+#' n_latent = 5
+#' sce = SingleCellExperiment(assays = list(counts = floor(matrix(rnorm(n_row*n_col), ncol=n_col)) + 4))
+#' rownames(sce) = as.factor(1:n_row)
+#' colnames(sce) = c(1:n_col)
+#' sce$cell = colnames(sce)
+#' sce$sample = floor(runif(n = n_col , min = 1 , max = 5))
+#' sce$type = ifelse(sce$sample %in% c(1,2) , "ref" , "query")
+#' reducedDim(sce , "reduced_dim") = matrix(rnorm(n_col*n_latent), ncol=n_latent)
+#' sce = assign_neighbourhoods(sce, reducedDim.name = "reduced_dim")
+#' de_stat = de_test_all_hoods(sce , condition.id = "type" , discard_not_perturbed_hoods = FALSE)
 de_test_all_hoods = function(sce ,
                              genes = rownames(sce) ,
                              sample.id = "sample",
                              condition.id ,
-                             discard_not_perturbed_hoods = T,
+                             discard_not_perturbed_hoods = c(TRUE,FALSE),
                              covariates = NULL ,
                              min_n_cells_per_sample = 2,
                              gene_selection = "all" ,
@@ -45,48 +56,54 @@ de_test_all_hoods = function(sce ,
   if (discard_not_perturbed_hoods){
     sce = .filter_not_perturbed_hoods(sce)
   }
-  nhoods_sce = nhoods(sce)
+  if (!isFALSE(sce)){
+    nhoods_sce = nhoods(sce)
 
-  if (gene_selection == "all"){
-    summed = summarizeAssayByGroup(counts(sce), colData(sce)[,c("condition.id", "sample.id")])
-    summed = SingleCellExperiment(list(counts=assay(summed, "sum")), colData=colData(summed))
-    y <- DGEList(counts(summed), samples=colData(summed), lib.size = colSums(counts(summed)))
-    keep <- filterByExpr(y, group=summed$sample.id , min.count = min.count , min.total.count = round(min.count * 1.5))
-    genes = names(keep)[keep]
-    sce = sce[genes , ]
+    if (gene_selection == "all"){
+      summed = summarizeAssayByGroup(counts(sce), colData(sce)[,c("condition.id", "sample.id")])
+      summed = SingleCellExperiment(list(counts=assay(summed, "sum")), colData=colData(summed))
+      y <- DGEList(counts(summed), samples=colData(summed), lib.size = colSums(counts(summed)))
+      keep <- filterByExpr(y, group=summed$sample.id , min.count = min.count , min.total.count = round(min.count * 1.5))
+      genes = names(keep)[keep]
+      sce = sce[genes , ]
+    }
+
+    # get de stat for all hoods
+    de_stat = lapply(colnames(nhoods_sce) , function(hood.id){
+      out = de_test_single_hood(sce , genes = genes , nhoods_sce = nhoods_sce, hood.id = hood.id,
+                          sample.id = sample.id, condition.id = condition.id, covariates = covariates,
+                          min_n_cells_per_sample = min_n_cells_per_sample ,
+                          gene_selection = gene_selection , min.count = min.count)
+      return(out)
+    })
+    de_stat = do.call(rbind , de_stat)
+
+    # add Nhood
+    nhood_meta = data.frame(Nhood = 1:ncol(nhoods_sce) , Nhood_id = colnames(nhoods_sce))
+    de_stat = merge(de_stat , nhood_meta , all.x = T)
+
+    # add spatial correction
+    ## get weights from nhoods_sce
+    intersect_mat <- crossprod(nhoods_sce)
+    t.connect <- unname(rowSums(intersect_mat))
+    weights<- 1/unlist(t.connect)
+
+    unq.genes = unique(de_stat$gene)
+
+    de_stat_w_correction = lapply(unq.genes , function(gene){
+      current.stat = de_stat[de_stat$gene == gene , ]
+      current.stat = current.stat[order(current.stat$Nhood) , ]
+      current.stat$SpatialFDR = spatial_pval_adjustment(weights = weights , nhoods_sce = nhoods_sce , pvalues = current.stat$PValue)
+      current.stat$gene = gene
+      return(current.stat)
+    })
+    de_stat_w_correction = do.call(rbind , de_stat_w_correction)
+    return(de_stat_w_correction)
   }
-
-  # get de stat for all hoods
-  de_stat = lapply(colnames(nhoods_sce) , function(hood.id){
-    out = de_test_single_hood(sce , genes = genes , nhoods_sce = nhoods_sce, hood.id = hood.id,
-                        sample.id = sample.id, condition.id = condition.id, covariates = covariates,
-                        min_n_cells_per_sample = min_n_cells_per_sample ,
-                        gene_selection = gene_selection , min.count = min.count)
-    return(out)
-  })
-  de_stat = do.call(rbind , de_stat)
-
-  # add Nhood
-  nhood_meta = data.frame(Nhood = 1:ncol(nhoods_sce) , Nhood_id = colnames(nhoods_sce))
-  de_stat = merge(de_stat , nhood_meta , all.x = T)
-
-  # add spatial correction
-  ## get weights from nhoods_sce
-  intersect_mat <- crossprod(nhoods_sce)
-  t.connect <- unname(rowSums(intersect_mat))
-  weights<- 1/unlist(t.connect)
-
-  unq.genes = unique(de_stat$gene)
-
-  de_stat_w_correction = lapply(unq.genes , function(gene){
-    current.stat = de_stat[de_stat$gene == gene , ]
-    current.stat = current.stat[order(current.stat$Nhood) , ]
-    current.stat$SpatialFDR = spatial_pval_adjustment(weights = weights , nhoods_sce = nhoods_sce , pvalues = current.stat$PValue)
-    current.stat$gene = gene
-    return(current.stat)
-  })
-  de_stat_w_correction = do.call(rbind , de_stat_w_correction)
-  return(de_stat_w_correction)
+  else {
+    message("No hoods were tested.")
+    return(F)
+  }
 }
 
 
@@ -113,6 +130,21 @@ de_test_all_hoods = function(sce ,
 #' @importFrom tibble rownames_to_column
 #' @importFrom scuttle summarizeAssayByGroup
 #' @examples
+#' require(SingleCellExperiment)
+#' require(miloR)
+#' n_row = 500
+#' n_col = 100
+#' n_latent = 5
+#' sce = SingleCellExperiment(assays = list(counts = floor(matrix(rnorm(n_row*n_col), ncol=n_col)) + 4))
+#' rownames(sce) = as.factor(1:n_row)
+#' colnames(sce) = c(1:n_col)
+#' sce$cell = colnames(sce)
+#' sce$sample = floor(runif(n = n_col , min = 1 , max = 5))
+#' sce$type = ifelse(sce$sample %in% c(1,2) , "ref" , "query")
+#' reducedDim(sce , "reduced_dim") = matrix(rnorm(n_col*n_latent), ncol=n_latent)
+#' sce = assign_neighbourhoods(sce, reducedDim.name = "reduced_dim")
+#' nhoods_sce = nhoods(sce)
+#' de_stat = de_test_single_hood(sce , nhoods_sce = nhoods_sce, hood.id = colnames(nhoods_sce)[1] , sample.id = "sample" , condition.id = "type")
 de_test_single_hood = function(sce , genes = rownames(sce) , nhoods_sce , hood.id , sample.id , condition.id , covariates = NULL,
                                min_n_cells_per_sample = 2 , gene_selection = "all" , min.count = 5){
   sce = sce[genes , ]
@@ -188,7 +220,6 @@ de_test_single_hood = function(sce , genes = rownames(sce) , nhoods_sce , hood.i
 #' @importFrom SingleCellExperiment colData
 #' @importFrom miloR buildNhoodGraph nhoodIndex nhoods
 #'
-#' @examples
 .filter_not_perturbed_hoods <- function(sce){
 
   # hardcoded for now - maybe change later
@@ -226,7 +257,8 @@ de_test_single_hood = function(sce , genes = rownames(sce) , nhoods_sce , hood.i
     return(sce_milo)
   }
   else {
-    message("All hoods have AUC less than selected threshold. No DE to be performed.")
+    stop("All hoods have AUC less than selected threshold. No DE to be performed.")
+    return(F)
   }
 
 }
