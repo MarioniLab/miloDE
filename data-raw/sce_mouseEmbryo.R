9d6674d9a882a7a898b85addc068aa7f571ee816
amissarova <alsu.missarova.32@gmail.com>
2022-12-23 14:12:20 +0000

    upd

--- data-raw/sce_mouseEmbryo.R
+++ data-raw/sce_mouseEmbryo.R
@@ -9,71 +9,31 @@ library(MouseGastrulationData)
 library(geneBasisR)
 
 # subset of 3 cell types
-cts = c("Spinal cord" , "Cardiomyocytes" , "NMP")
-
-# load ref WT
-samples = c(17,29,36,37)
-sce_ref <- lapply(samples , function(sample){
-  out <- EmbryoAtlasData(samples = sample)
-  out <- out[, out$celltype %in% cts]
-  return(out)
-})
-sce_ref <- do.call(cbind , sce_ref)
-# add tal1 and type ids
-sce_ref$type = "wt"
-sce_ref$tal1 = "plus"
-
+cts = c("Endothelium" , "PGC", "Neural crest")
 
 # load chimera Tal1
-sce_chimera = Tal1ChimeraData()
+sce = Tal1ChimeraData()
 # select CTs
-sce_chimera = sce_chimera[, sce_chimera$celltype.mapped %in% cts]
+sce = sce[, sce$celltype.mapped %in% cts]
 # delete row for tomato
-sce_chimera = sce_chimera[!rownames(sce_chimera) == "tomato-td" , ]
-
-# add chimera_ prefix to avoid conclusion
-colnames(sce_chimera) = paste0("chimera_" , colnames(sce_chimera) )
-sce_chimera$cell = paste0("chimera_" ,sce_chimera$cell)
-sce_chimera$sample = paste0("chimera_" , sce_chimera$sample )
-
-# add type and tal1
-sce_chimera$type = "chimera"
-sce_chimera$tal1 = ifelse(sce_chimera$tomato , "minus" , "plus")
-
-# sample same number of cells that is to sce
-sce_chimera = sce_chimera[, sample(colnames(sce_chimera) , ncol(sce))]
-
-# concatenate
-## order rownames, check that dss are concatenable
-sce_ref = sce_ref[order(rownames(sce_ref)) , ]
-sce_chimera = sce_chimera[order(rownames(sce_chimera)) , ]
-print(mean(rownames(sce_ref) == rownames(sce_chimera)))
-
-## concatenate
-sce = SingleCellExperiment(list(counts = cbind(counts(sce_ref) , counts(sce_chimera))))
-## add metadata
-sce$cell = c(sce_ref$cell , sce_chimera$cell)
-sce$celltype = c(sce_ref$celltype , sce_chimera$celltype.mapped)
-sce$sample = c(sce_ref$sample , sce_chimera$sample)
-sce$type = c(sce_ref$type , sce_chimera$type)
-sce$tal1 = c(sce_ref$tal1 , sce_chimera$tal1)
-
-# add rowdata
-rowData(sce) = rowData(sce_ref)
+sce = sce[!rownames(sce) == "tomato-td" , ]
 
 # add logcounts
 sce = scuttle::logNormCounts(sce)
 
 
 # add covariates
-sce$stage = "E8.5"
-sce$sex = sapply(1:ncol(sce) , function(i) ifelse(sce$sample[i] %in% c(36,37,"chimera_3" , "chimera_4") , "F" , "M"))
+sce$sex = sapply(1:ncol(sce) , function(i) ifelse(sce$sample[i] %in% c("3" , "4") , "F" , "M"))
 set.seed(32)
 sce$toy_cov_1 = sample(1:5 , ncol(sce),1)
 
 # select only 1000 genes
-sce = retain_informative_genes(sce , n = 1000)
-sce_mouseEmbryo = sce
+sce = retain_informative_genes(sce , n = 500)
+sce_mouseEmbryo = SingleCellExperiment(list(counts = counts(sce)),
+                                       colData = colData(sce),
+                                       rowData = rowData(sce)
+                                       )
+reducedDim(sce_mouseEmbryo , "pca.corrected") = reducedDim(sce , "pca.corrected")
 usethis::use_data(sce_mouseEmbryo , overwrite = TRUE , compress = "xz")
 
 
--- tests/testthat/test-assign_neighbourhoods.R
+++ tests/testthat/test-assign_neighbourhoods.R
@@ -1,17 +1,9 @@
-# context("Testing assign_hoods")
+# context("Testing assign_neighbourhoods")
 library(miloDE)
 
 # load data
 data("sce_mouseEmbryo", package = "miloDE")
-sce_mouseEmbryo = add_embedding(sce = sce_mouseEmbryo , genes = NULL, n_hvgs = 500,
-                                      assay_type = "logcounts" ,
-                                      reduction_type = "MNN" ,
-                                      reducedDim_name = "pca_mnn",
-                                      sample_id = "sample",
-                                      cells_ref = colnames(sce_mouseEmbryo)[sce_mouseEmbryo$type == "wt"],
-                                      cells_query = colnames(sce_mouseEmbryo)[sce_mouseEmbryo$type == "chimera"],
-                                      cell_id = NULL ,
-                                      d = 10)
+
 test_that("Wrong input gives errors", {
   # k should be positive
   expect_error(assign_neighbourhoods(sce_mouseEmbryo , k = 0, prop = 0.2, order = 2, filtering = T, reducedDim_name = "pca_mnn", k_init = 50, d = 30),
@@ -20,15 +12,15 @@ test_that("Wrong input gives errors", {
   )
   # reduced dim should be in reducedDimNames
   expect_error(assign_neighbourhoods(sce = sce_mouseEmbryo , k = 10, prop = 0.2, order = 2, filtering = T, reducedDim_name = "pca_azimuth", k_init = 50, d = 30),
-               "reducedDim_name should be in reducedDimNames(sce). If you do not have embedding precalculated, run first 'add_embedding'.",
+               "reducedDim_name should be in reducedDimNames(sce).",
                fixed=TRUE
   )
   # order can be only 1 or 2
-  expect_error(assign_neighbourhoods(sce_mouseEmbryo , k = 10, prop = 0.2, order = 3, filtering = T, reducedDim_name = "pca_azimuth", k_init = 50, d = 30),
+  expect_error(assign_neighbourhoods(sce_mouseEmbryo , k = 10, prop = 0.2, order = 3, filtering = T, reducedDim_name = "pca.corrected", k_init = 50, d = 30),
                "Check order - should be either 1 (standard kNN-graph) or 2 (2nd-order kNN-graph)",
                fixed=TRUE
   )
-  expect_error(assign_neighbourhoods(sce_mouseEmbryo , k = 10, prop = 0.2, order = "a", filtering = T, reducedDim_name = "pca_azimuth", k_init = 50, d = 30),
+  expect_error(assign_neighbourhoods(sce_mouseEmbryo , k = 10, prop = 0.2, order = "a", filtering = T, reducedDim_name = "pca.corrected", k_init = 50, d = 30),
                "Check order - should be either 1 (standard kNN-graph) or 2 (2nd-order kNN-graph)",
                fixed=TRUE
   )
